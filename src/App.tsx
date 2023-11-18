b6fbd5e2f530aa12d5fb6c6de1d13b02ff896872
Pouya Saremian <pooya.saremiyan@gmail.com>
2023-11-18 23:47:06 +0330

    Title Component Added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,67 @@
 import { useReducer } from "react";
 import Style from "./App.module.scss";
+import Title from "./components/title/title";
 import gameReducer, { boardInitializer } from "./reducers/game-reducer";
 
 function App() {
   const [state, dispatch] = useReducer(gameReducer, {}, boardInitializer);
 
   return (
-    <div className={Style["game__container"]}>
-      <div className={Style["game__squares"]}>
-        {state.squares.map((el) => (
-          <div
-            key={el.id}
-            className={Style["game__square"]}
-            style={el.taken ? { background: "blue" } : {}}
-          >
+    <>
+      <Title />
+      <div className={Style["game__container"]}>
+        <div className={Style["game__squares"]}>
+          {state.squares.map((el) => (
             <div
-              className={Style["game__square--left"]}
-              style={el.positions[0].taken ? { borderColor: "blue" } : {}}
-              onClick={() =>
-                dispatch({
-                  type: "LINE_SELECTED",
-                  payload: { id: el.id, position: el.positions[0].id },
-                })
-              }
-            ></div>
-            <div
-              className={Style["game__square--top"]}
-              style={el.positions[1].taken ? { borderColor: "blue" } : {}}
-              onClick={() =>
-                dispatch({
-                  type: "LINE_SELECTED",
-                  payload: { id: el.id, position: el.positions[1].id },
-                })
-              }
-            ></div>
-            <div
-              className={Style["game__square--right"]}
-              style={el.positions[2].taken ? { borderColor: "blue" } : {}}
-              onClick={() =>
-                dispatch({
-                  type: "LINE_SELECTED",
-                  payload: { id: el.id, position: el.positions[2].id },
-                })
-              }
-            ></div>
-            <div
-              className={Style["game__square--bottom"]}
-              style={el.positions[3].taken ? { borderColor: "blue" } : {}}
-              onClick={() =>
-                dispatch({
-                  type: "LINE_SELECTED",
-                  payload: { id: el.id, position: el.positions[3].id },
-                })
-              }
-            ></div>
-          </div>
-        ))}
+              key={el.id}
+              className={Style["game__square"]}
+              style={el.taken ? { background: "blue" } : {}}
+            >
+              <div
+                className={Style["game__square--left"]}
+                style={el.positions[0].taken ? { borderColor: "blue" } : {}}
+                onClick={() =>
+                  dispatch({
+                    type: "LINE_SELECTED",
+                    payload: { id: el.id, position: el.positions[0].id },
+                  })
+                }
+              ></div>
+              <div
+                className={Style["game__square--top"]}
+                style={el.positions[1].taken ? { borderColor: "blue" } : {}}
+                onClick={() =>
+                  dispatch({
+                    type: "LINE_SELECTED",
+                    payload: { id: el.id, position: el.positions[1].id },
+                  })
+                }
+              ></div>
+              <div
+                className={Style["game__square--right"]}
+                style={el.positions[2].taken ? { borderColor: "blue" } : {}}
+                onClick={() =>
+                  dispatch({
+                    type: "LINE_SELECTED",
+                    payload: { id: el.id, position: el.positions[2].id },
+                  })
+                }
+              ></div>
+              <div
+                className={Style["game__square--bottom"]}
+                style={el.positions[3].taken ? { borderColor: "blue" } : {}}
+                onClick={() =>
+                  dispatch({
+                    type: "LINE_SELECTED",
+                    payload: { id: el.id, position: el.positions[3].id },
+                  })
+                }
+              ></div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/title/title.tsx
@@ -0,0 +1,6 @@
+import Style from "./title.module.scss";
+const Title = () => {
+  return <h1 className={Style["title"]}>Box & Line Game</h1>;
+};
+
+export default Title;
